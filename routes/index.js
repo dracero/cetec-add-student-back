af41fdb57c324d3c5cefe646d74e4860305b70be
Manuel Collazo <collazo.manuel.28@gmail.com>
2022-08-17 20:53:34 -0300

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var {
 } = require('../controllers/student_controller.js');
 
 router.all('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', process.env.FRONT_URL);
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
